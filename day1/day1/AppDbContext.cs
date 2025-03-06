b17d0340eb4b6ac99bb25d6e019aee8ee30afe06
Meghal2103 <meghal2103@outlook.com>
2025-03-06 16:02:34 +0530

    day1

---
+++ day1/day1/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using day1;
+using Microsoft.EntityFrameworkCore;
+
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseSqlServer("Server=MEGHAL-F15\\SQLEXPRESS;Database=E_Commerce_System;Trusted_Connection=True;TrustServerCertificate=True");
+        }
+    }
+}
--- day1/day1/Program.cs
+++ day1/day1/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +10,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+using var context = new ApplicationDbContext();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
