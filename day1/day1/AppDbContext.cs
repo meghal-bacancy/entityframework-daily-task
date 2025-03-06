fae58df43436797c8e871d40bc21e3fcce450b46
Meghal2103 <meghal2103@outlook.com>
2025-03-06 15:51:48 +0530

    day1

---
+++ day1/day1/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+internal class AppDbContext : DbContext
+{   public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<day1.Student> Students { get; set; }
+}
